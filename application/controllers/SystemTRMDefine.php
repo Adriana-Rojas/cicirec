26759b0c7ee07a269c3517b851b8ad6a271f66f7
Adriana-Rojas <apatriciarojas@ucundinamarca.edu.co>
2022-05-05 15:38:53 -0500

    error

--- application/controllers/SystemTRMDefine.php
+++ application/controllers/SystemTRMDefine.php
@@ -42,7 +42,7 @@ class SystemTRMDefine extends CI_Controller
 			// Pinto la pantalla
 			$data['mainPage'] = $mainPage;
 
-			$date = isset($_GET['date']) ? $_GET['date'] : date("Y-m-d");
+			/*$date = isset($_GET['date']) ? $_GET['date'] : date("Y-m-d");
 			error_reporting(0);
 			try {
 				//Se incluye de forma obligatoria la libreria nusoap
@@ -72,7 +72,10 @@ class SystemTRMDefine extends CI_Controller
 			// Cargo los parametros
 			// Cargo la lista de paises
 			//$data['valor'] = $this->FunctionsGeneral->getFieldFromTable("ADM_TRM", "VALOR", 1);
-			$data['valor'] = $tms;
+			$data['valor'] = $tms;*/
+			$data['valor'] = $this->FunctionsGeneral->getFieldFromTable("ADM_TRM", "VALOR", 1);
+			
+			echo "<script>console.log('Console: " . $data['valor'] . "' );</script>";
 
 			// Pinto plantilla principal
 			$this->load->view('system/trmDefine/board', $data);
@@ -96,6 +99,7 @@ class SystemTRMDefine extends CI_Controller
 		 * Guardo la informaci�n de los parametros dentro del sistema
 		 */
 		$mainPage = "SystemTRMDefine/board";
+		echo "<script>console.log('Console guardo : " . "' );</script>";
 		if ($this->FunctionsAdmin->validateSession($mainPage)) {
 			$id = 1;
 			// ----------------------- Par�metros generales -------------------------- //
@@ -111,6 +115,58 @@ class SystemTRMDefine extends CI_Controller
 		} else {
 			// Retorno a la p�gina principal
 			header("Location: " . base_url());
+		}
+	}
+
+	public function consultatrm()
+	{
+		/**
+		 * Guardo la informaci�n de los parametros dentro del sistema
+		 */
+		$mainPage = "SystemTRMDefine/board";
+		echo "<script>console.log('Console entro a consulta: "  . "' );</script>";
+		if ($this->FunctionsAdmin->validateSession($mainPage)) {
+			// ----------------------- Par�metros generales -------------------------- //
+			$date = isset($_GET['date']) ? $_GET['date'] : date("2022-05-04");
+			error_reporting(0);
+			try {
+				//Se incluye de forma obligatoria la libreria nusoap
+				require_once('application/lib/nusoap.php');
+
+				//Se realiza la conexion con el webservice de SuperFinanciera a través de SOAP
+				$soap = new soapclient("https://www.superfinanciera.gov.co/SuperfinancieraWebServiceTRM/TCRMServicesWebService/TCRMServicesWebService?WSDL", array(
+					'soap_version'   => SOAP_1_1,
+					'trace' => 1,
+					"location" => "http://www.superfinanciera.gov.co/SuperfinancieraWebServiceTRM/TCRMServicesWebService/TCRMServicesWebService",
+				));
+				//Se llama el metodo queryTCRM identificada en el WDSL
+				$response = $soap->queryTCRM(array('tcrmQueryAssociatedDate' => $date));
+				$response = $response->return;
+				//Se verifica si la respuesta del WebService es correcta
+				if ($response->success) {
+					
+					echo "Fechaa:  $date<br/>";					
+					$tms=(int)$response->value;
+					echo "Tipo de cambio:  $tms<br/>";
+				} else {
+				}
+			} catch (Exception $e) {
+			}
+
+			echo "<script>console.log('Console: " .  $tms . "' );</script>";
+
+			// Cargo los parametros
+			// Cargo la lista de paises
+			//$data['valor'] = $this->FunctionsGeneral->getFieldFromTable("ADM_TRM", "VALOR", 1);
+			$data['valor'] = $tms;
+
+			// Redirecciono la p�gina
+			$mainPage = "SystemTRMDefine/board";
+			redirect(base_url() . $mainPage);
+		} else {
+			// Retorno a la p�gina principal
+			header("Location: " . base_url());
+			echo "<script>console.log('Console eror : " .  $tms . "' );</script>";
 		}
 	}
 }
--- application/views/system/trmDefine/board.php
+++ application/views/system/trmDefine/board.php
@@ -23,33 +23,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <!-- ============================================================== -->
 <!-- Start Page Content -->
 <!-- ============================================================== -->
-<form class=" form-horizontal" role="form" action="<?= base_url() ?>SystemTRMDefine/saveParameters" id="form_sample_3" method="post" autocomplete="off">
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="card">
-				<div class="card-body">
-					<h5 class="card-title"> Definici&oacute;n de TRM
-						<small class="font-gray">Identifique el valor en pesos (COP) que aplica a ka TRM de acuerdo con el cambio actual</small>
-					</h5>
+<div class="row">
+	<div class="col-sm-12">
+		<div class="card">
+			<div class="card-body">
+				<h5 class="card-title"> Definici&oacute;n de TRM
+					<small class="font-gray">Identifique el valor en pesos (COP) que aplica a ka TRM de acuerdo con el cambio actual</small>
+				</h5>
 
-					<div class="form-group">
-						<label class="col-md-12" for="valor">Valor *</label>
-						<div class="col-md-12">
-							<input type="number" class="form-control" id="valor" name="valor" value="<?= $valor ?>" placeholder="Ejemplo. 3000" min="2500" max="4000">
-							<div class="form-control-feedback"> </div>
-						</div>
+				<div class="form-group">
+					<label class="col-md-12" for="valor">Valor *</label>
+					<div class="col-md-12">
+						<input type="number" class="form-control" id="valor" name="valor" value="<?= $valor ?>" placeholder="Ejemplo. 3000" min="2500" max="6000">
+						<div class="form-control-feedback"> </div>
 					</div>
-
-					<h5 style="color: navy;">TRM  de hoy <?= $valor ?></h5>
-
-					<!--  <button type="submit" class="btn btn-inverse waves-effect waves-light">Cancel</button>  -->
-
 				</div>
+
+				<form class=" form-horizontal" role="form" action="<?= base_url() ?>SystemTRMDefine/consultatrm" id="form_sample_3" method="post" autocomplete="off">
+					<h5 style="color: navy;">TRM de hoy <?= $valor ?></h5>
+					<div class="col-md-12">
+						<input class="form-control input-limit-datepicker" type="date" name="date" id="date" />
+						<div class="form-control-feedback"> </div>
+						<button type="submit" class="btn btn-info btn-rounded waves-effect waves-light m-r-10 pull-right">Consultar TRM</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+</div>
 
-	<!-- Bot�n de envio de formulario -->
+<!-- Bot�n de envio de formulario -->
+<form class=" form-horizontal" role="form" action="<?= base_url() ?>SystemTRMDefine/saveParameters" id="form_sample_3" method="post" autocomplete="off">
+
 	<div class="row">
 		<div class="col-sm-12">
 			<button type="submit" class="btn btn-info btn-rounded waves-effect waves-light m-r-10 pull-right">Enviar</button>
@@ -58,8 +63,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 			<br>
 		</div>
 	</div>
-	<!-- FIN Bot�n de envio de formulario -->
 </form>
+<!-- FIN Bot�n de envio de formulario -->
+
 <!-- ============================================================== -->
 <!-- End PAge Content -->
 <!-- ============================================================== -->
